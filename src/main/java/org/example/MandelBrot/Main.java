27540f18d560232a51d83fad608f3ae488779f61
Marc <mlopesab@myuax.com>
2023-10-13 19:12:04 +0200

    Main

---
+++ src/main/java/org/example/MandelBrot/Main.java
@@ -0,0 +1,9 @@
+package org.example.MandelBrot;
+
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        GUIMandelbrot.mostrarMain();
+    }
+}
